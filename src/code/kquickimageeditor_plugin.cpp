b38650f365ffd05a9268d8a65a6ff300d1f0d298
Volker Krause <vkrause@kde.org>
2023-11-29 17:47:45 +0100

    Remove manual QML type registration and URL resolution
    For QML files listed in the qmldir file Qt usually does the right thing
    automatically for us here.
    
    This fixes using this with Qt6 on Android.

--- src/code/kquickimageeditor_plugin.cpp
+++ src/code/kquickimageeditor_plugin.cpp
@@ -14,18 +14,8 @@
 
 void KQuickImageEditorPlugin::registerTypes(const char *uri)
 {
-#if defined(Q_OS_ANDROID)
-    QResource::registerResource(QStringLiteral("assets:/android_rcc_bundle.rcc"));
-#endif
-
     qmlRegisterType<ResizeHandle>(uri, 1, 0, "ResizeHandle");
     qmlRegisterType<ResizeRectangle>(uri, 1, 0, "ResizeRectangle");
     qmlRegisterType<ImageItem>(uri, 1, 0, "ImageItem");
     qmlRegisterType<ImageDocument>(uri, 1, 0, "ImageDocument");
-    qmlRegisterType(resolveFileUrl(QStringLiteral("BasicResizeHandle.qml")), uri, 1, 0, "BasicResizeHandle");
-    qmlRegisterType(resolveFileUrl(QStringLiteral("SelectionTool.qml")), uri, 1, 0, "SelectionTool");
-    qmlRegisterType(resolveFileUrl(QStringLiteral("SelectionHandle.qml")), uri, 1, 0, "SelectionHandle");
-    qmlRegisterType(resolveFileUrl(QStringLiteral("SelectionBackground.qml")), uri, 1, 0, "SelectionBackground");
-    qmlRegisterType(resolveFileUrl(QStringLiteral("CropBackground.qml")), uri, 1, 0, "CropBackground");
-    qmlRegisterType(resolveFileUrl(QStringLiteral("RectangleCutout.qml")), uri, 1, 0, "RectangleCutout");
 }
--- src/code/kquickimageeditor_plugin.h
+++ src/code/kquickimageeditor_plugin.h
@@ -15,20 +15,4 @@ public:
     void registerTypes(const char *uri) override;
 
 private:
-    QString resolveFilePath(const QString &path) const
-    {
-#if defined(Q_OS_ANDROID)
-        return QStringLiteral(":/android_rcc_bundle/qml/org/kde/kquickimageeditor/") + path;
-#else
-        return baseUrl().toLocalFile() + QLatin1Char('/') + path;
-#endif
-    }
-    QString resolveFileUrl(const QString &filePath) const
-    {
-#if defined(Q_OS_ANDROID)
-        return QStringLiteral("qrc:/android_rcc_bundle/qml/org/kde/kquickimageeditor/") + filePath;
-#else
-        return baseUrl().toString() + QLatin1Char('/') + filePath;
-#endif
-    }
 };
