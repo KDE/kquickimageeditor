dc9bdf9c5f797b553b869ab997abdabe4308762a
Noah Davis <noahadvs@gmail.com>
2026-07-01 09:04:41 -0400

    Test stack blur radius behavior
    Ensure all radius values don't crash the program,
    change the image when they should
    and don't change the image when they shouldn't.

--- autotests/stackblurtest.cpp
+++ autotests/stackblurtest.cpp
@@ -16,6 +16,7 @@ class StackBlurTest : public QObject
     Q_OBJECT
 private Q_SLOTS:
     void testFormats();
+    void testRadius();
 };
 
 void StackBlurTest::testFormats()
@@ -34,6 +35,87 @@ void StackBlurTest::testFormats()
         debugString = u"Got %1 for %2"_s.arg(QDebug::toString(result), QDebug::toString(format)).toStdString();
         QVERIFY2(result == PixelDifferent, debugString.c_str());
     }
+}
+
+void StackBlurTest::testRadius()
+{
+    using enum ImageComparison::Result;
+    using Px = decltype(StackBlur::minRadius());
+    // Make sure our basic assumptions are correct.
+    static_assert(std::is_same_v<decltype(StackBlur::maxRadius()), Px>);
+    static_assert(std::is_same_v<decltype(StackBlur::minRadius()), Px>);
+    static_assert(std::is_same_v<decltype(StackBlur::maxKernel()), Px>);
+    static_assert(std::is_same_v<decltype(StackBlur::minKernel()), Px>);
+    static_assert(StackBlur::maxRadius() > 0);
+    static_assert(StackBlur::minRadius() > 0);
+    static_assert(StackBlur::minRadius() < StackBlur::maxRadius());
+    static_assert(StackBlur::maxKernel() > 0);
+    static_assert(StackBlur::minKernel() > 0);
+    static_assert(StackBlur::minKernel() < StackBlur::maxKernel());
+    static_assert(StackBlur::maxKernel() / 2 == StackBlur::maxRadius());
+    static_assert(StackBlur::minKernel() / 2 == StackBlur::minRadius());
+    static_assert((StackBlur::minKernel() & 1) != 0); // is odd
+    static_assert((StackBlur::maxKernel() & 1) != 0);
+    static constexpr auto IMAGE_FORMAT = QImage::Format_ARGB32_Premultiplied;
+    static constexpr auto supportedImageFormats = StackBlur::supportedImageFormats();
+    static_assert(std::find(supportedImageFormats.begin(), supportedImageFormats.end(), IMAGE_FORMAT) != supportedImageFormats.end());
+
+    QImage image;
+
+    auto setSize = [&](int width, int heigth = -1) {
+        image = QImage{width, heigth >= 0 ? heigth : width, IMAGE_FORMAT};
+        randomizeImageData(image);
+    };
+    // We don't just put QCOMPARE or QVERIFY in here because then the program
+    // will tell you something failed in the lambda when a test fails.
+    auto testRadius = [&](Radius<Px> radius) {
+        QImage testImage = image;
+        auto [radiusX, radiusY] = radius;
+        StackBlur::blur(testImage, radiusX, radiusY);
+        return ImageComparison::compare<QRgb>(image, testImage);
+    };
+
+    // No change to verify since the QImages are null,
+    // but make sure this doesn't crash
+    setSize(0);
+    QCOMPARE(testRadius(1), BothNull);
+
+    // Test too small to blur
+    setSize(1);
+    QCOMPARE(testRadius(1), ExactSamePixels);
+
+    // Test minimum
+    setSize(StackBlur::minKernel()); // minimum blurrable size
+    QCOMPARE(testRadius(StackBlur::minRadius()), PixelDifferent);
+    // Test slightly out in case of off by 1 error
+    QCOMPARE(testRadius(StackBlur::minRadius() + 1), PixelDifferent);
+    // Test 0 radius
+    QCOMPARE(testRadius(0), ExactSamePixels);
+    // Test negative
+    QCOMPARE(testRadius(-1), ExactSamePixels);
+    QCOMPARE(testRadius(-100), ExactSamePixels);
+    QCOMPARE(testRadius(std::numeric_limits<int>::lowest()), ExactSamePixels);
+    // Test max constrained by image size.
+    QCOMPARE(testRadius(StackBlur::maxRadius()), PixelDifferent);
+
+    // Add extra size so the radius definitely isn't limited by image size
+    setSize(StackBlur::maxKernel() + 2);
+    // Test max radius unconstrained by size.
+    QCOMPARE(testRadius(StackBlur::maxRadius()), PixelDifferent);
+    // Test out of max radius by 1
+    QCOMPARE(testRadius(StackBlur::maxRadius() + 1), PixelDifferent);
+    // Test max int
+    QCOMPARE(testRadius(std::numeric_limits<int>::max()), PixelDifferent);
+
+    // Test X/Width-only
+    setSize(StackBlur::maxKernel() + 2, 1);
+    QCOMPARE(testRadius({8, 0}), PixelDifferent);
+    QCOMPARE(testRadius({0, 8}), ExactSamePixels);
+
+    // Test Y/Height-only
+    setSize(1, StackBlur::maxKernel() + 2);
+    QCOMPARE(testRadius({0, 8}), PixelDifferent);
+    QCOMPARE(testRadius({8, 0}), ExactSamePixels);
 }
 
 QTEST_GUILESS_MAIN(StackBlurTest)
